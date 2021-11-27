052f1e58ce387c1976d4d088a8416b905a4c768c
Abdulrahman <abdulrahmanmvp93@gmail.com>
2021-11-27 19:22:09 +1100

    coins appear in paginated list

--- cb-crypto-app/src/components/Layout/Landing.js
+++ cb-crypto-app/src/components/Layout/Landing.js
@@ -1,135 +1,117 @@
-import React, { Component } from 'react'
-import Pagination from '../Pagination/Pagination';
+import React, {useState, useEffect} from 'react'
+import ReactPaginate from 'react-paginate';
 import axios from 'axios';
-import CryptoCoins from '../CryptoCoins/CryptoCoins';
 
-class Landing extends Component {
+function Landing() {
+  const [offset, setOffset] = useState(0);
+  const [coinsList, setData] = useState([]);
+  const [perPage] = useState(3);
+  const [pageCount, setPageCount] = useState(0);
 
-    //states to store crypto data
-    constructor(){
-        super();
-        this.state = {
-            allCryptoCoins:[], 
-            currentCryptoCoins:[], 
-            currenPage:null, 
-            totalPages:null,
-            isLoaded:false,
-            hasError:false
-        }
-    }
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
 
-    //fetch crypto data information
-    getCryptoCoinsData(){
-        const cryptoCoinData = axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false');
-        return cryptoCoinData;
-    }
-    //when page is launched, invoke getCrypotCoindData to get all data
-    //and assign state of allCryptoCoins
-    componentDidMount(){
-        this.getCryptoCoinsData().then(response =>{
-            this.setState({allCryptoCoins: response.data});
-            console.log(response.data);
-        })
-        //set loading and error state
-        .then(() => {
-            this.setState({isLoaded:true}); 
-        },
-        () => {
-            this.setState({hasError: true});
-        });
-    }
+  const [query, setquery] = useState("");
+  const [searchParam] = useState(["name", "symbol"]);
 
-    //display a limited number of coins in a page
-    //withing a sepcified set of pages
-    onPageChange = data => {
-        const { allCryptoCoins } = this.state;
-        const{currentPage, totalPages, pageLimit} = data;
-        //call the api with special paramters
-        axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&page=${currentPage}&limit=${pageLimit}&sparkline=false`)
-            .then(response => {
-                const currentCryptoCoins = response.data.id;
-                this.setState({currentPage, currentCryptoCoins, totalPages});
-            })
-            //set loading and error state
-            .then(() => {
-                    this.setState({isLoaded:true});   
-            },
-            () => {
-                this.setState({hasError:true});
-            });
-        }  
-
-    render() {
-        const{isLoaded, allCryptoCoins,currentCryptoCoins,currentPage,totalPages} = this.state;
-        const totalCryptoCoins = allCryptoCoins.length;
-        //if api is empty
-        if(totalCryptoCoins === 0){
-
-            return <div>There are no crypto coins in this api!</div>
-        }
-        //if sever is taking too long to respond
-        else if(!isLoaded){
-            return <div>Loading.....</div>
-        }
-        //if the server returned back any error
-        else if(this.state.hasError === true){
-            return <div>Sorry, there seems to be a problem with the server!</div>
-        }
-        //show coins information
-        else{
+  //function to filter items based on given paramters
+  function search(coins){
+    return coins.filter((item) => {
+        return searchParam.some((newCoinList) => {
             return (
-                <div className="container mb-5">
-                    <div className="row d-flex flex-row py-5">
-            
-                    <div className="w-100 px-4 py-5 d-flex flex-row flex-wrap align-items-center justify-content-between">
-                        <div className="d-flex flex-row align-items-center">
-            
-                        <h2>
-                            <strong className="text-secondary">{totalCryptoCoins}</strong> Coin Types
-                        </h2>
-            
-                        { currentPage && (
-                            <span className="current-page d-inline-block h-100 pl-4 text-secondary">
-                            Page <span className="font-weight-bold">{ currentPage }</span> / <span className="font-weight-bold">{ totalPages }</span>
-                            </span>
-                        ) }
-            
-                        </div>
-                        {/* create pagination tabe with page numbers */}
-                        <div className="d-flex flex-row py-4 align-items-center">
-                            <Pagination totalRecords={totalCryptoCoins} pageLimit={18} pageNeighbours={1} onPageChanged={this.onPageChanged} />
-                        </div>
-                    </div>
-            
-                        {/* show crypto coins */}
-                        {allCryptoCoins.map((crypto) => (
-                            <article className="card cards" key={crypto.callingCodes}>
-                            <div className="card-image bookImg">
-                                <img src={crypto.image} />
-                            </div>
-                            <div className="card-content">
-                                <h2 className="card-name">{crypto.name}</h2>
-                                <ol className="card-list">
-                                    <li>
-                                        id:{" "}
-                                        <span>{crypto.id}</span>
-                                    </li>
-                                    <li>
-                                        Price: <span>{crypto.current_price}</span>
-                                    </li>
-                                    <li>
-                                        Market Cap Rank: <span>{crypto.market_cap_rank}</span>
-                                    </li>
-                                </ol>
-                            </div>
-                        </article>
-                        ))}
-            
-                    </div>
-                </div>        
-            );
+                item[newCoinList]
+                    .toString()
+                    .toLowerCase()
+                    .indexOf(query.toLocaleLowerCase()) > -1      
+                );
+            });
+         });
         }
+
+
+  const getCryptoCoinsData = async() => {
+        const cryptoCoins = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
+        const data = cryptoCoins.data;
+                    const slice = data.slice(offset, offset + perPage)
+                    const coins = (slice.map(cc => 
+                        <div key={cc.id}>
+                            <article className="card cards" key={cc.callingCodes}>
+                            <div className="card-image bookImg">
+                                <img src={cc.image} alt={cc.name} />
+                            </div>
+                                <div className="card-content">
+                                    <h2 className="card-name">{cc.name}</h2>
+                                    <ul className="card-list">
+                                        <li>
+                                            Symbol:{" "}
+                                            <span>{cc.symbol}</span>
+                                        </li>
+                                        <li>
+                                            Price:
+                                                <span>{cc.current_price}</span>
+                                        </li>
+                                        <li>
+                                            MCR: 
+                                            <span>{cc.market_cap_rank}</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </article>
+                        </div>))
+                    setData(coins)
+                    setPageCount(Math.ceil(data.length / perPage))
     }
+    const handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        setOffset(selectedPage + 1)
+    };
+
+    useEffect(() => {
+        getCryptoCoinsData()
+        .then(
+            () => {
+                setIsLoaded(true);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+    }, [offset])
+
+    if(error){
+        return <div>Error : {error.message}</div>
+    }
+    else if(!isLoaded){
+        return <div>Loading...</div>
+    }
+    else{
+        return (
+            <div>
+                {coinsList}
+                <input 
+                    type="text" 
+                    className="search-bar" 
+                    placeholder="Filter items"
+                    value = {query}
+                    onChange={(e) => setquery(e.target.value)}
+                        />
+                <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
+            </div>  
+        );
+    }
+        
 }
 
 export default Landing;
