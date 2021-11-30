caa56057e652772af838957cb87e0a131668ed5a
Abdulrahman <abdulrahmanmvp93@gmail.com>
2021-11-30 15:42:06 +1100

    over-haul of the application

--- cb-crypto-app/src/App.js
+++ cb-crypto-app/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
-import Home from './components/Home';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/Layout/NavBar";
+import CoinPagination from "./components/CoinPagination";
+import CoinFilter from "./components/CoinFilter";
 
 function App() {
   return (
-    <Home />
+    <div>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={CoinPagination} />
+          <Route exact path="/filter" component={CoinFilter} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
