0a674214c2c37580f6c1a79af5dd70f86a46c936
Abdulrahman <abdulrahmanmvp93@gmail.com>
2021-11-30 16:35:12 +1100

    clean code base

--- cb-crypto-app/src/components/Tests/__test__/CoinPagination.test.js
+++ cb-crypto-app/src/components/Tests/__test__/CoinPagination.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import CoinPagination from '../../CoinPagination'
 
-
+//tests component redering without crashing
 it("Renders without crashing", ()=> {
     const div = document.createElement("div");
         render(<CoinPagination />, div)
